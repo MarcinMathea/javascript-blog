d0a53ed6996e8e14d394cd13cc1c31ca856f179f
Marcin Mathea <mar.mathea@gmail.com>
2019-10-14 18:44:15 +0200

    removing article links and articles

--- js/script.js
+++ js/script.js
@@ -3,12 +3,20 @@
 const titleClickHandler = function(event){
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /*[DONE] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* add class 'active' to the clicked link */
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+  /*[IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  /*[DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
